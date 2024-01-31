d61c28e246a18a0c8b28e952f306a702b053d615
mldkyt <mldkyt@proton.me>
2024-01-31 13:29:59 +0100

    It actually does things now

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ Rewritten in Python by @mldkyt on 29th January 2024
 import json
 import random
 import subprocess
+import codecs
 import discord
 from discord.ext import commands as discord_commands
 
-with open('config.json', 'w', encoding='utf8') as f:
+with open('config.json', 'r', encoding='utf8') as f:
     config = json.load(f)
 
 if 'BOT_TOKEN' not in config:
@@ -24,16 +25,14 @@ async def log_to_logs(message: str):
         return
     await channel.send(message)
 
-@client.slash_command
+@client.slash_command(name="ping", description="fuck off")
 async def ping(ctx: discord.Interaction):
-    """fuck off""" # this is the description of the command. it's very cool and creative
     await ctx.response.send_message("fuck off")
 
-@client.slash_command
-@discord_commands.bot_has_permissions(perms=discord.Permissions.manage_roles)
+@client.slash_command(name="rr", description="bruh idk")
+@discord_commands.bot_has_permissions(manage_roles=True)
 @discord_commands.guild_only()
 async def rr(ctx: discord.Interaction):
-    """bruh idk""" # my brain is not braining :3
     rand = random.randint(1, 6)
     if rand == 4:
         await ctx.response.send_message('*BANG*')
@@ -43,54 +42,51 @@ async def rr(ctx: discord.Interaction):
 
     await ctx.response.send_message('*click*')
 
-@client.slash_command
+@client.slash_command(name="avatar", description="Get avatar of an user")
 async def avatar(ctx: discord.Interaction, user: discord.User):
-    """Get avatar of an user"""
     if not user.avatar:
-        await ctx.response.send_message('User doensn\'t have an avatar')
+        await ctx.response.send_message('User doesn\'t have an avatar')
         return
 
     await ctx.response.send_message(user.avatar.url)
 
-@client.slash_command
+@client.slash_command(name="hex", description="Convert text to hex")
 async def hex(ctx: discord.Interaction, text: str):
-    """Convert text to hex"""
-    converted = text.encode('hex')
+    converted = text.encode(encoding='hex')
     await ctx.response.send_message(converted)
 
-@client.slash_command
+@client.slash_command(name="run", description="OWNER ONLY: Run a command on the host machine (is very safe)")
 async def run(ctx: discord.Interaction, text: str):
-    """OWNER ONLY: Run a command on the host machine (is very safe)"""
+    await ctx.response.defer()
+    
     if ctx.user.id != config['OWNER_TAG']:
         await ctx.response.send_message('You are not allowed to use this command')
         return
 
     program = subprocess.run(text, shell=True, capture_output=True)
     if program.returncode != 0:
-        await ctx.response.send_message(f'ERROR: {program.stderr.decode("utf8")}')
+        await ctx.followup.send(content=f'ERROR: {program.stderr.decode("utf8")}')
         return
     
-    await ctx.response.send_message(program.stdout.decode('utf8'))
+    await ctx.followup.send(content=program.stdout.decode('utf8'))
 
 
 @client.event
 async def on_ready():
-    """When bot ready"""
     print(f'Ready! Logged in as {client.user}')
     log_to_logs(f'{client.user} is in your walls (bot started)')
 
 
 @client.event
 async def on_interaction_error(ctx: discord.Interaction, error: Exception):
-    """When an error occurs in a slash command"""
     if isinstance(error, discord_commands.NoPrivateMessage):
         await ctx.response.send_message('This command cannot be used in DMs')
         return
-    
+
     if isinstance(error, discord_commands.BotMissingPermissions):
         await ctx.response.send_message('I don\'t have the required permissions to run this command')
         return
-    
+
     raise error
 
-client.run(config['token'])
+client.run(config['BOT_TOKEN'])
